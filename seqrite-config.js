83024b4693301e7939f8ecea6d924871d1f2e28b
Praveen06041994 <praveentamil90@gmail.com>
2026-02-11 16:51:38 +0530

    Add files via upload

---
+++ seqrite-config.js
@@ -0,0 +1,211 @@
+
+let userLang = (navigator.language || (navigator).userLanguage || 'en');
+console.log("Browser Language Code: ", userLang);
+window.klaroConfig = {
+  "consentManagerId": "698c572214bd690a630a4f23",
+  "uuid": "b2da521c-5bb3-4a33-81b7-c43520efc96c",
+  "domain": "https://demo-shp.qhtpl.com/cmapi",
+  "version": 1,
+  "elementID": "SEQRITE",
+  lang: userLang,
+  "storageMethod": "cookie",
+  "storageName": "https://www.facebook.com/",
+  "cookieExpiresAfterDays": 365,
+  "default": false,
+  "mustConsent": false,
+  "acceptAll": true,
+  "hideDeclineAll": false,
+  "hideLearnMore": false,
+  "disablePoweredBy": false,
+  "translations": {
+    "english": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Marketing": {
+          "description": "<p>These cookies are used by our advertising partners to deliver personalized ads and measure the effectiveness of marketing campaigns. They help provide relevant promotions based on user interests and browsing behavior. </p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "en": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Marketing": {
+          "description": "<p>These cookies are used by our advertising partners to deliver personalized ads and measure the effectiveness of marketing campaigns. They help provide relevant promotions based on user interests and browsing behavior. </p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "zz": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Marketing": {
+          "description": "<p>These cookies are used by our advertising partners to deliver personalized ads and measure the effectiveness of marketing campaigns. They help provide relevant promotions based on user interests and browsing behavior. </p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    }
+  },
+  "styling": {
+    "theme": [
+      "light",
+      "custom"
+    ]
+  },
+  "htmlTexts": true,
+  "embedded": false,
+  "declarative": false,
+  "groupByPurpose": true,
+  "hideToggleAll": false,
+  "appName": "FBCookie",
+  "description": "We respects your privacy and is committed to protecting your personal data.",
+  "companyName": "FBCookie",
+  "companyAddress": " ",
+  "services": [
+    {
+      "name": "datr",
+      "title": "datr",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": true,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "datr"
+      ]
+    },
+    {
+      "name": "fr",
+      "title": "fr",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": true,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "fr"
+      ]
+    },
+    {
+      "name": "sb",
+      "title": "sb",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": true,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "sb"
+      ]
+    }
+  ]
+};
+showCookie();
